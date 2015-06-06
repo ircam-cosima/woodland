a41e440beed987184d2ac42766a4ba8a6937b660
jipodine <jipodine@users.noreply.github.com>
2015-06-06 10:29:10 +0200

    more sounds

--- src/common/files.es6.js
+++ src/common/files.es6.js
@@ -17,12 +17,16 @@ files.sounds = [
   'BbCl-ord-C4-mf_c.mp3',
   'basse_2_faD_sto_c.mp3',
   'tenor_4_sol--_sto_c.mp3',
+  'soprano_6_do_stei_67_c.mp3',
   'cymbalum_ForteDo1_c.mp3',
   'Mridangam.c_c.mp3',
   'Mridangam.d_c.mp3',
   'Mridangam.h_c.mp3',
   'Mridangam.i_c.mp3',
-  'Mridangam.l_c.mp3'
+  'Mridangam.l_c.mp3',
+  'growl_40-3_c.mp3',
+  'growl_43-2_c.mp3',
+  'tettigonia-cantans_c.mp3'
 ];
 
 module.exports = exports = files;
